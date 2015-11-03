a8a9126fa81c2a5f54bf1d575d2d77540ed30fba
Ken Okabe <ken@design-fun.com>
2015-11-04 07:02:31 +0900

    to functicon

--- html/scripts.js
+++ html/scripts.js
@@ -1,14 +1,8 @@
-$(function(){
 
+
+$(function(){
   $("#btnSay").click(function(){
-    $(".ajax_res_work").text("クリックされました。");
-    $.post(
-      'cgi-bin/mqtt_web_if.rb',
-      {'led1':6, 'led3':5},
-      function(data){
-        $('.ajax_res').html(data);
-      }
-    );
+    displayAlert();
   });
 
   $(".chkled").click(function(){
@@ -31,7 +25,15 @@ $(function(){
       }
     );
   });
-
-
-
 });
+
+function displayAlert(){
+  $(".ajax_res_work").text("クリックされました。");
+  $.post(
+    'cgi-bin/mqtt_web_if.rb',
+    {'led1':6, 'led3':5},
+    function(data){
+      $('.ajax_res').html(data);
+    }
+  );
+}
