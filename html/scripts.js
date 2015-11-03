524367f65c55abb40fef1df64f330370fe1b11a2
Ken Okabe <ken@design-fun.com>
2015-11-03 15:30:06 +0900

    ajax checkbox

--- html/scripts.js
+++ html/scripts.js
@@ -11,6 +11,27 @@ $(function(){
     );
   });
 
+  $(".chkled").click(function(){
+    var code={'led1': '', 'led2':'','led3':''};
+    if($("#chkled1").prop('checked')){
+      code["led1"]="on";
+    }
+    if($("#chkled2").prop('checked')){
+      code["led2"]="on";
+    }
+    if($("#chkled3").prop('checked')){
+      code["led3"]="on";
+    }
+
+    $.post(
+      'cgi-bin/mqtt_web_if.rb',
+      code,
+      function(data){
+        $('.ajax_res').html(data);
+      }
+    );
+  });
+
 
 
 });
